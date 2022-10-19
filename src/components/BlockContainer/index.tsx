dbf55250bbe295381edc54292cf885c1366deb8b
LAPTOP-KM0D1F16\kaind <3494688@qq.com>
2022-10-19 10:25:13 +0800

    调整了BlockContainer组件中Card组件的样式

--- src/components/BlockContainer/index.tsx
+++ src/components/BlockContainer/index.tsx
@@ -2,11 +2,21 @@ import React from 'react';
 import { Card } from 'antd';
 
 interface IBlockContainer {
+  title?: string;
+  extra?: React.ReactNode;
   children: React.ReactNode;
 }
 
-const BlockContainer: React.FC<IBlockContainer> = ({ children }) => {
-  return <Card className=" shadow-md rounded-md">{children}</Card>;
+const BlockContainer: React.FC<IBlockContainer> = ({
+  title,
+  extra,
+  children,
+}) => {
+  return (
+    <Card title={title} extra={extra} className="shadow rounded-md">
+      {children}
+    </Card>
+  );
 };
 
 export default BlockContainer;
