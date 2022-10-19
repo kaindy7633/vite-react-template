41607ac79ac8428c67587f295fbdb7e5d1cb392a
LAPTOP-KM0D1F16\kaind <3494688@qq.com>
2022-10-19 16:10:52 +0800

    完善404页面，新增403，500页面

--- src/pages/403.tsx
+++ src/pages/403.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button, Result } from 'antd';
 
-const NotFound: React.FC = () => {
+const AuthFaild: React.FC = () => {
   const navigate = useNavigate();
 
   return (
     <Result
-      status="404"
-      title="404"
-      subTitle="您访问的页面未找到, 请返回首页!"
+      status="403"
+      title="403"
+      subTitle="抱歉, 您没有权限访问此页面!"
       extra={
         <Button type="primary" onClick={() => navigate('/')}>
           返回
@@ -19,4 +19,4 @@ const NotFound: React.FC = () => {
   );
 };
 
-export default NotFound;
+export default AuthFaild;
--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button, Result } from 'antd';
 
-const NotFound: React.FC = () => {
+const ServerError: React.FC = () => {
   const navigate = useNavigate();
 
   return (
     <Result
-      status="404"
-      title="404"
-      subTitle="您访问的页面未找到, 请返回首页!"
+      status="500"
+      title="500"
+      subTitle="抱歉, 服务端发生异常, 请返回!"
       extra={
         <Button type="primary" onClick={() => navigate('/')}>
           返回
@@ -19,4 +19,4 @@ const NotFound: React.FC = () => {
   );
 };
 
-export default NotFound;
+export default ServerError;
