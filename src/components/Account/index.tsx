deaab2b2c64eda9b52b5fc6d3f0023d7181d9be7
LAPTOP-KM0D1F16\kaind <3494688@qq.com>
2022-10-20 15:16:12 +0800

    修改管理员下拉菜单

--- src/components/Account/index.tsx
+++ src/components/Account/index.tsx
@@ -1,13 +1,58 @@
 import React from 'react';
-import { Avatar } from 'antd';
+import { Avatar, Dropdown, Menu } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
+
+const menu = (
+  <Menu
+    items={[
+      {
+        key: '1',
+        label: (
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.antgroup.com"
+          >
+            帮助
+          </a>
+        ),
+      },
+      {
+        key: '2',
+        label: (
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.aliyun.com"
+          >
+            个人中心
+          </a>
+        ),
+      },
+      {
+        key: '3',
+        label: (
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.luohanacademy.com"
+          >
+            退出登录
+          </a>
+        ),
+      },
+    ]}
+  />
+);
 
 const Account: React.FC = () => {
   return (
-    <div className="w-full h-full flex flex-row flex-nowrap justify-center items-center gap-1">
-      <Avatar icon={<UserOutlined />} size="small" />
-      <span>管理员登录</span>
-    </div>
+    <Dropdown overlay={menu} placement="bottomRight" arrow>
+      <div className="w-full h-full flex flex-row flex-nowrap justify-center items-center gap-1">
+        <Avatar icon={<UserOutlined />} size="small" />
+        <span>管理员登录</span>
+      </div>
+    </Dropdown>
   );
 };
 
