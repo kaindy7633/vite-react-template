051c4fca71d71ba06e953935d10f33da7c9b9f1a
liuzhen <3494688@qq.com>
2022-12-02 16:24:12 +0800

    新增系统语言设置

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ConfigProvider } from 'antd';
+import { Locale } from 'antd/lib/locale-provider';
+import zhCN from 'antd/lib/locale/zh_CN';
 import { RouterGurad, routes } from '@/routes';
 import Loading from '@/pages/Loading';
 import { useThemeStore } from './store';
@@ -8,8 +10,11 @@ const App: React.FC = () => {
   // 当前默认主题
   const { prefix } = useThemeStore();
 
+  // 当前系统语言
+  const [locale] = useState<Locale>(zhCN);
+
   return (
-    <ConfigProvider prefixCls={prefix}>
+    <ConfigProvider prefixCls={prefix} locale={locale}>
       <React.Suspense fallback={<Loading />}>
         <div className={`App ${prefix} h-[100vh]`}>{RouterGurad(routes)}</div>
       </React.Suspense>
