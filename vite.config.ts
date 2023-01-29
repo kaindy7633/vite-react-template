96827a0e50ae620dc96e286b2d19f768649ce2f9
liuzhen <3494688@qq.com>
2023-01-29 14:21:14 +0800

    升级vite对React的支持插件，vitejs/plugin-react-swc，并在配置中启用

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv } from 'vite';
 import vitePluginImp from 'vite-plugin-imp';
 import { viteMockServe } from 'vite-plugin-mock';
-import react from '@vitejs/plugin-react';
+import react from "@vitejs/plugin-react-swc";
 // 动态主题切换工具
 import { resolve } from 'path';
 
