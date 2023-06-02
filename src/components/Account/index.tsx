c2c5a1683da2b43a0d1f3358c8f8e7444eb0cbcf
liuzhen <3494688@qq.com>
2023-06-02 15:05:45 +0800

    修复侧边栏样式问题，替换overlary变量

--- src/components/Account/index.tsx
+++ src/components/Account/index.tsx
@@ -1,53 +1,49 @@
 import React from 'react';
-import { Avatar, Dropdown, Menu } from 'antd';
+import { Avatar, Dropdown, MenuProps } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
 
-const menu = (
-  <Menu
-    items={[
-      {
-        key: '1',
-        label: (
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.antgroup.com"
-          >
-            帮助
-          </a>
-        ),
-      },
-      {
-        key: '2',
-        label: (
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.aliyun.com"
-          >
-            个人中心
-          </a>
-        ),
-      },
-      {
-        key: '3',
-        label: (
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.luohanacademy.com"
-          >
-            退出登录
-          </a>
-        ),
-      },
-    ]}
-  />
-);
+const items: MenuProps['items'] = [
+  {
+    key: '1',
+    label: (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.antgroup.com"
+      >
+        帮助
+      </a>
+    ),
+  },
+  {
+    key: '2',
+    label: (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.aliyun.com"
+      >
+        个人中心
+      </a>
+    ),
+  },
+  {
+    key: '3',
+    label: (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.luohanacademy.com"
+      >
+        退出登录
+      </a>
+    ),
+  },
+];
 
 const Account: React.FC = () => {
   return (
-    <Dropdown overlay={menu} placement="bottomRight" arrow>
+    <Dropdown menu={{ items }} placement="bottomRight" arrow>
       <div className="w-full h-full flex flex-row flex-nowrap justify-center items-center gap-1">
         <Avatar icon={<UserOutlined />} size="small" />
         <span>管理员登录</span>
--- src/components/Sider/index.tsx
+++ src/components/Sider/index.tsx
@@ -12,7 +12,7 @@ interface ISiderProps {
 const Sider: React.FC<ISiderProps> = ({ collapsed, setCollapsed }) => {
   return (
     <Layout.Sider
-      className="overflow-hidden fixed left-0 top-[48px] bottom-0"
+      className="overflow-hidden fixed left-0 top-0 bottom-0 bg-white"
       trigger={null}
       collapsible
       collapsed={collapsed}
