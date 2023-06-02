a79671807e7323431ec27c07dcbaeaa2ced7ccab
liuzhen <3494688@qq.com>
2023-06-02 11:12:06 +0800

    项目Logo统一使用 public 下的

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Layout } from 'antd';
-import Logo from '@/assets/logo.svg';
 import { APP_TITLE } from '@/constants';
 import Account from '@/components/Account';
 import HeaderSearch from '../HeaderSearch';
@@ -11,7 +10,7 @@ const Header: React.FC = () => {
   return (
     <Layout.Header className="text-white h-12 w-full pl-3 pr-4 flex items-center justify-between fixed left-0 top-0 right-0 z-50 shadow-md">
       <div className="h-full flex flex-row flex-nowrap items-center gap-3">
-        <img src={Logo} alt={APP_TITLE} className="w-6 h-6" />
+        <img src="/logo.svg" alt={APP_TITLE} className="w-6 h-6" />
         <span className="text-[16px]">{APP_TITLE}</span>
       </div>
       <div className="h-full flex flex-row flex-nowrap items-center gap-5">
