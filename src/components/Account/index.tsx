01ad5a2be4be53146269e55122458c36faa10f47
kaindy7633 <kaindy7633@gmail.com>
2024-09-04 18:17:00 +0800

    修复暗黑模式下用户下拉菜单图标样式颜色不正确的问题

--- src/components/Account/index.tsx
+++ src/components/Account/index.tsx
@@ -1,56 +1,94 @@
 import React from 'react';
-import { Avatar, Dropdown, MenuProps, Typography } from 'antd';
+import {
+  theme as AntdTheme,
+  Avatar,
+  Dropdown,
+  MenuProps,
+  Typography,
+} from 'antd';
+import { useThemeStore } from '@/store';
 import { UserOutlined } from '@ant-design/icons';
 import RemixIcon from '@/components/RemixIcon';
 
-const items: MenuProps['items'] = [
-  {
-    key: '1',
-    icon: <RemixIcon icon="ri-questionnaire-line" />,
-    label: (
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://www.antgroup.com"
-      >
-        帮助
-      </a>
-    ),
-  },
-  {
-    key: '2',
-    icon: <RemixIcon icon="ri-id-card-line" />,
-    label: (
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://www.aliyun.com"
-      >
-        个人中心
-      </a>
-    ),
-  },
-  {
-    key: '3',
-    icon: <RemixIcon icon="ri-logout-circle-line" />,
-    label: (
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://www.luohanacademy.com"
-      >
-        退出登录
-      </a>
-    ),
-  },
-];
-
 const Account: React.FC = () => {
+  const { theme } = useThemeStore();
+
+  const items: MenuProps['items'] = [
+    {
+      key: '1',
+      icon: (
+        <RemixIcon
+          icon="ri-questionnaire-line"
+          color={
+            theme.algorithm === AntdTheme.defaultAlgorithm || !theme.algorithm
+              ? undefined
+              : '#FFFFFF'
+          }
+        />
+      ),
+      label: (
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.antgroup.com"
+        >
+          帮助
+        </a>
+      ),
+    },
+    {
+      key: '2',
+      icon: (
+        <RemixIcon
+          icon="ri-id-card-line"
+          color={
+            theme.algorithm === AntdTheme.defaultAlgorithm || !theme.algorithm
+              ? undefined
+              : '#FFFFFF'
+          }
+        />
+      ),
+      label: (
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.aliyun.com"
+        >
+          个人中心
+        </a>
+      ),
+    },
+    {
+      key: '3',
+      icon: (
+        <RemixIcon
+          icon="ri-logout-circle-line"
+          color={
+            theme.algorithm === AntdTheme.defaultAlgorithm || !theme.algorithm
+              ? undefined
+              : '#FFFFFF'
+          }
+        />
+      ),
+      label: (
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.luohanacademy.com"
+        >
+          退出登录
+        </a>
+      ),
+    },
+  ];
+
   return (
     <Dropdown menu={{ items }} placement="bottomRight" arrow>
       <div className="w-full h-full flex flex-row flex-nowrap justify-center items-center gap-1">
         <Avatar icon={<UserOutlined />} size="small" />
-        <Typography>管理员登录</Typography>
+        <Typography className="inline-block leading-[48px] w-[4rem] h-full truncate">
+          Admin
+        </Typography>
       </div>
     </Dropdown>
   );
